554ab1c51e564e894e3e81db48c60136f9b461e3
mxs <1842027542@qq.com>
2025-01-04 18:40:53 +0800

    update chapter_10

--- src/com/mxs/static_/StaticExercise02.java
+++ src/com/mxs/static_/StaticExercise02.java
@@ -1,24 +1,24 @@
-//package com.mxs.static_;
-//
-//public class StaticExercise02 {
-//    public static void main(String[] args) {
-//        System.out.println("Number of total is" + Person.getTotalPerson());
-//        Person person = new Person();
-//        System.out.println("Number of total is" + Person.getTotalPerson());
-//    }
-//}
-//
-//class Person {
-//    private int id;
-//    private static int total = 0;
-//
-//    public static int getTotalPerson() {
-////        id++;
-//        return total;
-//    }
-//
-//    public Person() {
-//        total++;
-//        id = total;
-//    }
-//}
+package com.mxs.static_;
+
+public class StaticExercise02 {
+    public static void main(String[] args) {
+        System.out.println("Number of total is" + Person.getTotalPerson());
+        Person person = new Person();
+        System.out.println("Number of total is" + Person.getTotalPerson());
+    }
+}
+
+class Person {
+    private int id;
+    private static int total = 0;
+
+    public static int getTotalPerson() {
+//        id++;
+        return total;
+    }
+
+    public Person() {
+        total++;
+        id = total;
+    }
+}
--- src/com/mxs/static_/StaticExercise03.java
+++ src/com/mxs/static_/StaticExercise03.java
@@ -3,16 +3,16 @@ package com.mxs.static_;
 public class StaticExercise03 {
 }
 
-class Person {
+class Persons {
     private int id;
     private static int total = 0;
 
     public static void setTotalPerson(int total) {
 //        this.total = total;
-        Person.total = total;
+        Persons.total = total;
     }
 
-    public Person() {
+    public Persons() {
         total++;//3
         id = total;
     }
@@ -20,7 +20,7 @@ class Person {
 
 class TestPerson{
     public static void main(String[] args) {
-        Person.setTotalPerson(3);
-        new Person();
+        Persons.setTotalPerson(3);
+        new Persons();
     }
 }
